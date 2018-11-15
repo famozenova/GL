791202d5459bedc0684284e7304519bf873d4652
famozenova <44798279+famozenova@users.noreply.github.com>
2018-11-15 14:50:10 +0100

    Update main.c
    Exercice 45 fini

--- src/main.c
+++ src/main.c
@@ -3,7 +3,45 @@
 
 #include "my_header.h"
 
+#include "TP_lib.h"
+
+
 int main(void)
+
 {
-    return 0;
+	 
+	 char n= LancerDe();
+	 unsigned long int resultat =0;
+	
+	
+	while(n!=1)
+	{
+		
+		  if(n%2==0)
+		   
+			 resultat = resultat + n;
+		  
+		  
+		  
+		  else if(n==3)
+		  
+			  resultat= resultat *2;
+			  
+		  
+		  
+		   else
+			   
+		   {
+			   if(resultat<2)
+				   
+				   resultat= 0;
+			   
+			   else 
+				   
+			   resultat = resultat -2;
+		   } 
+			
+		n= LancerDe();	   
+	}
+	return 0;
 }
