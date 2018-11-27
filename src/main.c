6de0c5698bea75d5eafdbcd9d18fa23f799c7f7e
florian amozenova <florian.amozenova@ge1.iut-cachan.u-psud.fr>
2018-11-27 09:49:16 +0100

    Nouveau changement

--- src/main.c
+++ src/main.c
@@ -5,6 +5,17 @@
 
 int main(void)
 {
+	unsigned char combi_ga[6] = {1 , 8 , 40 , 35 , 23 , 20};
+    unsigned char combi_jo[6] = {20 , 32 , 40 , 8 , 24 , 19}; 
+	unsigned char i , j;
+	
+	for ( i=0 , j=0 ; i<6 , j<6 ; i++ , j++)
+		
+	{
+	  
+		combi_ga[i] = combi_jo[j];
+	
+	}
 
 
     return 0;
