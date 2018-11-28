558962539de074e1be2863828c734ee10b263ce9
famozenova <44798279+famozenova@users.noreply.github.com>
2018-11-28 15:11:02 +0100

    Exercice 58 fini

--- src/main.c
+++ src/main.c
@@ -3,20 +3,49 @@
 
 #include "my_header.h"
 
+#include "TP_lib_suite.h"
+
+
+
 int main(void)
 {
-	unsigned char combi_ga[6] = {1 , 8 , 40 , 35 , 23 , 20};
-    unsigned char combi_jo[6] = {20 , 32 , 40 , 8 , 24 , 19}; 
-	unsigned char i , j;
 	
-	for ( i=0 , j=0 ; i<6 , j<6 ; i++ , j++)
-		
+	
+    unsigned char combi_ga[6];
+	unsigned char combi_jo[6] = { 35 , 42 , 22 , 45 , 2 , 20 } ;
+	unsigned char i , j , z , nb_bon = 0 , best_score = 0;
+  
+
+	for(z=0 ; z<100 ; z++)
 	{
-	  
-		combi_ga[i] = combi_jo[j];
+	initialiserTirage();
+	nb_bon = 0;
 	
+	
+	
+	for( j=0 ; j<6; j++)
+	{
+		combi_ga[j] = tirerNumero();
 	}
 
+	
+	for( i=0; i<6 ; i++)
+	{
+	 
+		for ( j=0 ; j<6 ; j++ )
+	 	{
+			if (combi_jo[i] == combi_ga[j])
+			nb_bon++;
+	 
+		 if( nb_bon > best_score)
+		  {
+		   best_score = nb_bon;
+		  }
+		}
+		
+	}
+	}
 
-    return 0;
+  return 0;
+	
 }
