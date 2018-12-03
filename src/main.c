ace9a36cfd4624cf54b67bdfc16af4e98634e9ba
famozenova <44798279+famozenova@users.noreply.github.com>
2018-12-03 19:36:02 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -17,6 +17,22 @@ int main(void)
 	 unsigned char bin[10]= {0,1,2,3,4,5,6,7,8,9};
 	 unsigned char i;
 	 
+	 short int sat[10] = {rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384};									
+	 short int y;
+	
+         int dist[10]= {0 , 2 , 8 , 19 , 20 , 25 , 38 , 45 , 65 , 80};
+         int z;
+	
+	
+         int a[5]= { 2 , 4 , 6 , 8 , 10} ; 
+         int b[5];
+	 int f , D=5;
+	 unsigned long long int S1 = 1;
+	 unsigned char n = 2;
+	
+	
+	 int miroir[10] = { 2 , 4 , 6 , 8 , 10 };
+	 int u , v ;
 	 
 	 
 
@@ -33,8 +49,7 @@ int main(void)
 	 }
 	 
 	 
-	 short int sat[10] = {rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384 , rand()-16384};									
-	 short int y;
+
 	 
 	 
 	 
@@ -57,8 +72,7 @@ int main(void)
 	 
 	   	
 	 
-	     int dist[10]= {0 , 2 , 8 , 19 , 20 , 25 , 38 , 45 , 65 , 80};
-		 int z;
+
 
 		 for(z=9 ; z>0 ; z=z-1)
 	  {
@@ -69,27 +83,29 @@ int main(void)
 		  
 	  }		
 	  
-	    int a[5]= { 2 , 4 , 6 , 8 , 10} ; 
-		int b[5];
-	    int f , D=5;
+
 		  
 		for(f=4 ; f>0 ; f= f-1)
 		{
+	while(n<50)
+	{
+	  S1 = S1*n;
+	  n++;
+	}
 
 			b[f]= a[D-f];
 			
 		}
 		
 		
-		int miroir[10] = { 2 , 4 , 6 , 8 , 10 };
-		int u , v ;
+
 		
 		for (u=9, v=0 ; v<5 , u>4 ;  v = v+1 ,u = u-1 )
 		{
 			miroir[u-1]=miroir[v+1];
 		}
 	 
-
+      return 0;
 	 return 0;
 }
 		
