a0d75004810a680485c54ff851b64b41c3208170
famozenova <44798279+famozenova@users.noreply.github.com>
2018-12-04 21:08:29 +0100

    Update main.c
    j'ai fini l'exercice 79

--- src/main.c
+++ src/main.c
@@ -2,17 +2,32 @@
 //
 
 #include "my_header.h"
+#include <userint.h>
+#include "stdio.h"
+
+
 
 int main(void)
 {
-    unsigned long long int S1 = 1;
-    unsigned char n = 2;
 
-    while (n<50)
-    {
-        S1 = S1 * n;
-        n = n + 1;
-    }
+char Nom[50] , prenom[50] , age[50];
+char message[100];
 
-    return 0;
+
+
+
+printf("Nom :");
+scanf ("%s" , Nom); 
+
+printf("Prenom :"); 
+scanf ("%s" , prenom);
+
+printf("Age :");  
+scanf ("%s" , age);
+
+sprintf(message, "Je m'appelle %s %s et j'ai %s ans" , Nom ,prenom , age);
+MessagePopup ("Présentation",message);
+
+
+ return 0;
 }
